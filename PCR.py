4978d7590fdebe1b6a16b49e798689b39236a520
Juan Sebastian Vera Rodriguez <UANDES\js.vera10@fisicaez12207l.ad.uniandes.edu.co>
2018-10-25 17:59:32 -0500

    Datos descargados

--- PCR.py
+++ PCR.py
@@ -1,3 +1,17 @@
 import numpy as np
 import matplotlib.pylab as plt
+import numpy.linalg
+import urllib2
+import time
 
+#Descarga de los datos desde la pagina web.
+
+
+
+descarga="http://ftp.cs.wisc.edu/math-prog/cpo-dataset/machine-learn/cancer/WDBC/WDBC.dat"
+a="WDBC.dat"
+down=urllib2.urlopen(descarga)
+archivo=file(a,"w")
+archivo.write(down.read())
+archivo.close()
+print 'Se descargo el archivo WDBC.dat'
