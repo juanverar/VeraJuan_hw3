a20923e1c420f6cfd7320a31a80be785ccb8db15
juanverar <40802443+juanverar@users.noreply.github.com>
2018-10-31 12:50:05 -0500

    Avance Fourier2D- Frecuencias

--- Fourier2D.py
+++ Fourier2D.py
@@ -6,26 +6,34 @@ import matplotlib.cm as cm
 from PIL import Image
 
 #Convertir la imagen a un array
-img=Image.open('arbol.png').convert('RGBA')
-arbol=np.asarray(img)
+imagen=Image.open('arbol.png').convert('RGBA')
+arbol=np.asarray(imagen)
 
 #Aplicar Fourirer al arreglo
-def fourier(imagen):
-	return fftpack.fft2(imagen)
+ff=fftpack.fft2(arbol)
+#Centrar las frecuecncias que tengan valor de 0 en la mitad del arreglo
+
+frec=np.fft.fftfreq(len(arbol),0.1)
+plt.plot(frec,ff[1])
+plt.show()
+
+
+centrado=np.fft.fftshift(ff)
+
 
 plt.figure()
-plt.imshow(np.abs(fourier(arbol)), cmap=cm.Greys_r, norm = LogNorm())
+plt.imshow(np.abs(centrado), cmap=cm.Greys_r, norm = LogNorm())
 plt.show()
-<<<<<<< HEAD
+
 print len(arbol[1])
 #def filtro():
 #	for i in range(len(arbol[0])):
 		
 #Aplicar la inversa de Fourier al arreglo flitrado.
-def inversa(imagen):
-	return fftpack.ifft2(imagen)
-arbol1 = plt.rcParams['image.cmap'] ='gray'
-plt.figure()
-plt.imshow(np.abs(inversa(fourier(arbol))), cmap=cm.Greys_r, norm = LogNorm())
-plt.show()
+#def inversa(imagen):
+#	return fftpack.ifft2(imagen)
+#arbol1 = plt.rcParams['image.cmap'] ='gray'
+#plt.figure()
+#plt.imshow(np.abs(inversa(fourier(arbol))), cmap=cm.Greys_r, norm = LogNorm())
+#plt.show()
 
