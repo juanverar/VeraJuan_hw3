967d79b3d817cafa27a7a4e751f3e37f50dbfdc9
juanverar <40802443+juanverar@users.noreply.github.com>
2018-10-31 18:37:06 -0500

    Fourier2D.py Terminado lindo y bello.

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,39 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import fftpack, ndimage
+from scipy import fftpack
 from matplotlib.colors import LogNorm
 import matplotlib.cm as cm
-from PIL import Image
-
-#Convertir la imagen a un array
-imagen=Image.open('arbol.png').convert('RGBA')
-arbol=np.asarray(imagen)
-
-#Aplicar Fourirer al arreglo
-ff=fftpack.fft2(arbol)
-#Centrar las frecuecncias que tengan valor de 0 en la mitad del arreglo
-
-frec=np.fft.fftfreq(len(arbol),0.1)
-plt.plot(frec,ff[1])
-plt.show()
 
 
-centrado=np.fft.fftshift(ff)
+#4.1 Convertir la imagen a un array
+imagen=plt.imread('arbol.png')
 
+#4.2 Aplicar Fourirer al arreglo y graficar
+ff=fftpack.fft2(imagen)
+#Centrar las frecuecncias que tengan valor de 0 en la mitad del arreglo.
+centrado=fftpack.fftshift(ff)
 
+#4.3 Aplicar filtro a la imagen para eliminar ruido periodico.
 plt.figure()
-plt.imshow(np.abs(centrado), cmap=cm.Greys_r, norm = LogNorm())
-plt.show()
+plt.imshow(np.abs(centrado), cmap=cm.autumn, norm = LogNorm())
+plt.title('Grafica de la transformada')
+plt.colorbar()
+plt.savefig('VeraJuan_FT2D.pdf')
 
-print len(arbol[1])
-#def filtro():
-#	for i in range(len(arbol[0])):
-		
+#A partir de prueba y error se determina el minimo y maximo, con estos valores se apica el filtro sobre la transformada centrada.
+
+def filtro():
+	for i in range(np.shape(centrado)[0]):
+		for j in range(np.shape(centrado)[0]):
+			if(centrado[i][j]<4400 and centrado[i][j]>2100):
+				centrado[i][j]=0
+	return centrado
 #Aplicar la inversa de Fourier al arreglo flitrado.
-#def inversa(imagen):
-#	return fftpack.ifft2(imagen)
-#arbol1 = plt.rcParams['image.cmap'] ='gray'
-#plt.figure()
-#plt.imshow(np.abs(inversa(fourier(arbol))), cmap=cm.Greys_r, norm = LogNorm())
-#plt.show()
+imagen2=filtro()
+nueva=fftpack.ifftshift(imagen2)
+iff=fftpack.ifft2(nueva)
 
+#Grafica de la transformada filtrada.
+plt.figure()
+plt.imshow(np.log(np.abs(imagen2)), cmap=cm.autumn, norm = LogNorm())
+plt.title('Grafica de la transformada filtrada')
+plt.colorbar()
+plt.savefig('VeraJuan_FT2D_filtrada.pdf')
+
+#Grafica de la imagen filtrada aplicada la inversa de la transformada.
+plt.figure()
+plt.imshow(np.abs(iff), cmap='gray', norm = LogNorm())
+plt.title('Grafica de la imagen filtrada')
+plt.show()
+#plt.savefig('VeraJuan_Imagen_filtrada.pdf')
