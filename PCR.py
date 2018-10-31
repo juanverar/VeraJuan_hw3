c9bed1f92a8dcbaa42b055506d71f648dadbd365
juanverar <40802443+juanverar@users.noreply.github.com>
2018-10-31 17:46:11 -0500

    PCA terminado - Falta discutir.

--- PCR.py
+++ PCR.py
@@ -16,7 +16,7 @@ print 'Se descargo el archivo WDBC.dat'
 
 A=np.genfromtxt('WDBC.dat',delimiter=",")
 AL=np.genfromtxt('WDBC.dat', dtype = str, delimiter=",", usecols=(1))
-
+AA=A[:,2:,]
 
 
 #for i in range(len(A[0])):
@@ -42,6 +42,41 @@ print cov
 
 val,vec = np.linalg.eig(cov)
 
-for i in range(len(val)):
-	print 'Para el autovalor=', val[i],'se tiene el autovector=',vec[i]
+#for i in range(len(val)):
+#	print 'Para el autovalor=', val[i],'se tiene el autovector=',vec[i]
+
+#2.4 Determinar cuales son los parametros mas importantes en base a lo obtenido antes.
+
+tipo=A[:,0]
+M=[]
+listM=[]
+B=[]
+listB=[]
+#Determinar cual dato corresponde a cada una de las dos opciones.
+for i in range(len(AL)):
+	if AL[i]=='M':
+		M.append(i)
+	if AL[i]=='B':
+		B.append(i)
+#Llenar dos arreglos con los datos correspondientes
+for i in range(len(M)):
+	listM.append(AA[M[i]])
+	listB.append(AA[B[i]])
+
+#Multiplicacion de los datos en sus dos categorias con sus respectivos autovectores.
+
+Malignos=np.matmul(listM,vec)
+Benignos=np.matmul(listB,vec)
+
+#Se plotean los datos segun su clasificacion y se muestran con diferentes colores en la grafica.
+
+plt.scatter(Benignos[:,0],Benignos[:,1],color='g',label="Benignos",marker='+')
+plt.scatter(Malignos[:,0],Malignos[:,1],color='r',label="Malignos",marker='+')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.title('PCA aplicado a diagnostico de pacientes con cancer.')
+plt.legend()
+plt.grid()
+plt.savefig('VeraJuan_PCA.pdf')
+
 
