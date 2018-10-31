a7c165e73edaffbcd8cedd5ad12ffc93411d871c
juanverar <40802443+juanverar@users.noreply.github.com>
2018-10-31 17:08:22 -0500

    Correccion PCR y calculo autovalores y autovectores.

--- PCR.py
+++ PCR.py
@@ -7,30 +7,41 @@ import time
 #2.1  Descarga de los datos desde la pagina web.
 
 descarga="http://ftp.cs.wisc.edu/math-prog/cpo-dataset/machine-learn/cancer/WDBC/WDBC.dat"
-a="WDBC.txt"
+a="WDBC.dat"
 down=urllib2.urlopen(descarga)
 archivo=file(a,"w")
 archivo.write(down.read())
 archivo.close()
 print 'Se descargo el archivo WDBC.dat'
-A=np.genfromtxt('WDBC.txt',delimiter=",")
-AL=np.genfromtxt('WDBC.txt',delimiter=",",dtype=str)
-print A
+
+A=np.genfromtxt('WDBC.dat',delimiter=",")
+AL=np.genfromtxt('WDBC.dat', dtype = str, delimiter=",", usecols=(1))
 
 
 
-for i in range(len(A[0])):
-	A=np.genfromtxt('WDBC.txt',delimiter=",",dtype=str, usecols=(i))
+#for i in range(len(A[0])):
+#	A=np.genfromtxt('WDBC.txt',delimiter=",",dtype=str, usecols=(i))
 
-#Convertir las letras de la columna 2 en variables numericas M=0 y B=1.
+#2.2 Implementacion de matriz de covarianza
+def matrizCov(dat):
+	n=np.shape(dat)[0]
+	var=np.shape(dat)[1]
+	matrix=np.ones([var,var])
+	for i in range(var):
+		for j in range(var):
+			mean1=np.mean(dat[:,i])
+			mean2=np.mean(dat[:,j])
+			matrix[i,j]=np.sum((dat[:,i]-mean1)*(dat[:,j]-mean2))/(n-1)
+	return matrix
 
-copy=np.copy(A[:,1:])
-def cambiostr():
-	for i in range(0,len(copy[:,0])):
-		if AL[i,1]=='B':
-			copy[i,0]=1.0
-		if Al[i,1]=='M':
-			copy[i,0]=0.0
-cambiostr()
+cov=matrizCov(A[:,2:,])
+print cov
 
+
+#2.3 Calculo de los autovectores y autovalores
+
+val,vec = np.linalg.eig(cov)
+
+for i in range(len(val)):
+	print 'Para el autovalor=', val[i],'se tiene el autovector=',vec[i]
 
