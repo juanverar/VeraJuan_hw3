e8ab41ad8a87f6b3ac6069968323f92c9e964638
juanverar <40802443+juanverar@users.noreply.github.com>
2018-10-31 19:54:32 -0500

    Ultimo avance 3.5 y 3.6

--- Fourier.py
+++ Fourier.py
@@ -24,13 +24,37 @@ N=len(x)
 dt=x[1]-x[0]
 f=fftfreq(N,dt)
 suma=np.linspace(0,0,N)
+
+
 for i in range(N):
 	for j in range(N):
-		suma[i]= suma[i]+(y[j]*np.exp((-1j)*2*np.pi*j*i/N))
+		suma[i]= suma[i]+(y[j]*np.exp((-1j)*2*np.pi*i*j/N))
 
 #3.4 Grafica de la transformada
 plt.figure()
-plt.plot(f,(np.abs(suma)/2))
+plt.plot(f,(np.abs(suma)/N))
 plt.title('Trasformada de Fourier')
 plt.grid()
 plt.savefig('VeraJuan_TF.pdf')
+
+#3.5 Frecuencias principales de la senal.
+fprin=[]
+for i in range(len(f)/2):
+	if((np.abs(suma[i])/N)>0.5):
+		fprin.append(np.abs(f[i]))
+print 'Las frecuencias principales de la senal son=', fprin
+
+#3.6 Filtro pasa bajos f=1000Hz
+tf=suma/N
+def pasabajos():
+	for i in range(len(suma/N)):
+		if (np.abs(f[i])>1000):
+			tf[i]= 0
+	return tf
+
+inv=ifft(pasabajos())
+plt.figure()
+plt.plot(x,inv)
+plt.title('Transformada al ser filtrada')
+plt.grid()
+plt.savefig('VeraJuan_filtrada.pdf')
